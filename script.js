ecc14cadb1151f92dc04f4d6140ca227b52112e6
Daylan-lewandowski <74326816+Daylan-lewandowski@users.noreply.github.com>
2021-01-20 15:24:19 -0600

    added some style

---
+++ script.js
@@ -0,0 +1,12 @@
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0');
+var yyyy = today.getFullYear();
+
+today = mm + '/' + dd + '/' + yyyy;
+document.write(today);
+
+var textarea = function() {
+    window.localStorage.setItem('textarea' , JSON.stringify(text));
+}
+
